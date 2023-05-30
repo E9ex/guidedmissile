cf417e07821cfbb24d7333ed0ebc4b4787511067
E9ex <egecansar@gmail.com>
2023-05-30 18:09:40 +0300

    missilefollow

---
+++ Assets/scripts/camerafollower.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollower : MonoBehaviour
+{
+    public Transform target;
+    public bool ischasing;
+    private launcher _launcher;
+
+    private void Awake()
+    {
+        _launcher = GetComponent<launcher>();
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            target = GameObject.FindObjectOfType<rocket>().gameObject.transform;
+            transform.parent = null;
+            ischasing = true;
+            _launcher.enabled = false;
+        }   
+    }
+
+    private void LateUpdate()
+    {
+        if (ischasing)
+        {
+            transform.forward = target.forward;
+            transform.position=Vector3.Lerp(transform.position,target.position,Time.deltaTime);
+        }
+    }
+}
