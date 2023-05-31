9764f5e492b0eae0a8b45ed04c35cbdbb0e20d96
Timuçin Topal <timucintopal@gmail.com>
2023-05-31 21:48:39 +0300

    [Changed] AudioManager

--- Assets/scripts/SoundManager.cs
+++ Assets/scripts/SoundManager.cs
@@ -2,12 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
+
+public enum SoundType
+{
+    Radar = 0,
+    TargetLock = 1,
+    MissileHit = 2,
+    MissileFire = 3
+    
+}
+
+[Serializable]
+public class Sound
+{
+    public AudioClip audioClip;
+    public SoundType soundType;
+    public float volume = 1.0f;
+}
 
 public class SoundManager : MonoBehaviour
 {
     public static SoundManager instance;
     private AudioSource _audioSource;
     [SerializeField] private AudioClip radarsesi, targetlock, missilecarpti, missileateslendi;
+
+    [SerializeField] List<Sound> sounds = new List<Sound>();
 
     private void Awake()
     {
@@ -16,19 +36,38 @@ public class SoundManager : MonoBehaviour
     }
     public void Radarsesi()
     {
-        _audioSource.PlayOneShot(radarsesi);
+        PlaySound(SoundType.Radar);
     }
     public void Targetlock()
     {
-        _audioSource.PlayOneShot(targetlock);
+        PlaySound(SoundType.TargetLock);
     }
     public void MissileLaunched()
     {
-        _audioSource.PlayOneShot(missileateslendi);
+        PlaySound(SoundType.MissileFire);
     }
     public void MissileHit()
     {
-        _audioSource.PlayOneShot(missilecarpti);
+        PlaySound(SoundType.MissileHit);
+    }
+
+    private Sound GetSound(SoundType soundType)
+    {
+        foreach (var sound in sounds)
+        {
+            if (soundType == sound.soundType)
+                return sound;
+        }
+        return null;
+    }
+
+    public void PlaySound(SoundType soundType)
+    {
+        var sound = GetSound(soundType);
+        if(sound == null) return;
+        
+        _audioSource.volume = sound.volume;
+        _audioSource.PlayOneShot(sound.audioClip);
     }
     
 }
